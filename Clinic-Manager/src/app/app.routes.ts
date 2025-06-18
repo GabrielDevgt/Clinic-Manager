b4c6754c5ed1dce5c98ce9bf0559bf486660cc60
GabrielDevgt <roldangabriel002@gmail.com>
2025-06-18 16:09:41 -0600

    feat:Implementation of basic components for patients, consultations, exams, etc., only handling routes without any data.

--- Clinic-Manager/src/app/app.routes.ts
+++ Clinic-Manager/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { PacientesComponent } from './pages/pacientes/pacientes.component';
+import { ConsultasComponent } from './pages/consultas/consultas.component';
+import { CitasComponent } from './pages/citas/citas.component';
+import { TratamientosComponent } from './pages/tratamientos/tratamientos.component';
+import { ExamenesComponent } from './pages/examenes/examenes.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component:HomeComponent},
+    { path: 'pacientes', component: PacientesComponent },
+    { path: 'consultas', component: ConsultasComponent },
+    { path: 'citas', component: CitasComponent },
+    { path: 'tratamientos', component: TratamientosComponent },
+    { path: 'examenes', component: ExamenesComponent },
+];
--- Clinic-Manager/src/app/components/navbar/navbar.component.ts
+++ Clinic-Manager/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor(private router: Router) {}
 
+  irA(url: string) {
+    this.router.navigate([url]);
+  }
 }
